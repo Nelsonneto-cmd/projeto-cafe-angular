3e0fcc00b657f8f93b914e19cd7133ad84fc5307
nelson <mambonelson62@gmail.com>
2026-06-27 10:40:35 +0100

    Organizando estrutura do projeto

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  // Alterado o título usando o Signal do Angular para o seu Café Bar
+  protected readonly title = signal('Café & Bar Dev');
+}
